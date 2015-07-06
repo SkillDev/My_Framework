3e75f846d662e87a75d279b5b0f569c0e0d48db9
skilldev <anton_m@epitech.eu>
2015-07-06 09:10:22 +0000

    ini commit

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php 
+/**
+*PHP version 5
+*
+*@category Sniffer
+*@package  Sniffer.Test
+*@author   ANTON Maicmelan <maicmelan.anton@epitech.eu>
+*@license  http://intra.epitech.eu  General Public License
+*@link     http://intra.epitech.eu
+*/
+// phpinfo();
+require_once dirname(__FILE__) . "/../lib/gamos/Core.php";
+gamos\Core::run();
+?>
