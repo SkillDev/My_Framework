592be47701b2b75f135ac7f39282cd4a85263171
skilldev <anton_m@epitech.eu>
2015-07-06 09:09:21 +0000

    test Model

---
+++ app/models/UserTable.php
@@ -0,0 +1,35 @@
+<?php
+/**
+*PHP version 5
+*File doc comment
+*@category Sniffer
+*@package  Sniffer.Test
+*@author   ANTON Maicmelan <maicmelan.anton@epitech.eu>
+*@license  http://intra.epitech.eu  General Public License
+*@link     http://intra.epitech.eu
+*/
+namespace app\models;
+use \lib\gamos\Model;
+/**
+*PHP version 5
+*Class doc domment
+*
+*@category Sniffer
+*@package  Sniffer.Test
+*@author   ANTON Maicmelan <maicmelan.anton@epitech.eu>
+*@license  http://intra.epitech.eu  General Public License
+*@link     http://intra.epitech.eu
+*/
+Class UserTable extends Model
+{
+    /**
+    * Process the return comment of this function comment.
+    *
+    * @return void
+    */
+    public function create()
+    {
+
+    }
+}
+?>
